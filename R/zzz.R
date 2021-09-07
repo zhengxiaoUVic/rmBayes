679b3753c7245641eef3b0e81abb7781477279bc
zhengxiaouvic <zhengxiao@uvic.ca>
2021-09-07 05:01:13 -0700

    Tried to improve the code coverage

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,4 @@
 .onAttach <- function(...) {
   packageStartupMessage("For execution on a local, multicore CPU with excess RAM we recommend calling\n",
                         "options(mc.cores = parallel::detectCores()).\n")
-  if (.Platform$OS.type == "windows") {
-    packageStartupMessage("Do not specify '-march=native' in 'LOCAL_CPPFLAGS' or a Makevars file")
-  }
 }
---
+++ tests/testthat/test-all.R
@@ -0,0 +1,28 @@
+# roughly 21 minutes to run on 672 scenarios
+
+skip_on_cran()
+options(warn=-1)
+
+test_that("executable", {
+  for (method in 0:6) {
+    for (var.equal in c(TRUE, FALSE)) {
+      for (design in c("within", "between")) {
+        for (treat in c("random", "fixed")) {
+          for (ht in c(1, 0.5, 1.1)) {
+            for (hb in c(1, 1.1)) {
+              for (diagnostics in c(FALSE, TRUE)) {
+                test <- rmHDI(data.wide = recall.wide, seed = 277, method = method,
+                              var.equal = var.equal, design = design, treat = treat,
+                              ht = ht, hb = hb, diagnostics = diagnostics)
+                expect_gte(length(test), 3)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+})
+
+options(warn=1)
