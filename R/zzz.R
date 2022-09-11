901cd77650d34bc8478c67428162a0b4a7b31933
zhengxiaouvic <zhengxiao@uvic.ca>
2022-09-10 23:58:04 -0700

    Deleted all...

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(...) {
-  packageStartupMessage("For execution on a local, multicore CPU with excess RAM we recommend calling\n",
-                        "options(mc.cores = parallel::detectCores()).\n")
-}
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(rmBayes)
-
-test_check("rmBayes")
