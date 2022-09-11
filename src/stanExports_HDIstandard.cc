6ba6cbde4c29fbaff90d75105ed29fd5191f28ce
zhengxiaouvic <zhengxiao@uvic.ca>
2022-09-11 01:27:00 -0700

    Started a new branch

--- src/stanExports_HDIstandard.cc
+++ src/stanExports_HDIstandard.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4HDIstandard_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_HDIstandard")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_HDIstandard")
 
     .constructor<SEXP,SEXP,SEXP>()
 
--- src/stanExports_HDIstandardFixed.cc
+++ src/stanExports_HDIstandardFixed.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4HDIstandardFixed_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_HDIstandardFixed")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_HDIstandardFixed")
 
     .constructor<SEXP,SEXP,SEXP>()
 
